b4f88417e793576c0cafbd890f7f97f2cb99c89a
Jfxmyy92 <wang.yanyue@northeastern.edu>
2025-02-26 20:48:30 -0800

    lab7

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        // Fake Exchange Class to replace the NYSE class
+        class FakeExchange implements Exchange {
+            @Override
+            public float rate(String origin, String target) {
+                return 0.85f;
+            }
+        }
+
         // Unit Test for Cash class
 
         // Test setup
-        Exchange exchange = new NYSE("someAccessKey");
+        Exchange exchange = new FakeExchange();
         Cash dollar = new Cash(exchange, 100);
         System.out.println("Dollar: " + dollar.toString());
+
 
         // Test 'in' method of Cash
         Cash euro = dollar.in("Euro");
